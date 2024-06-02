39aa872e0b491926bcd41071ecde1db0b4664149
Bianca Ribeiro <103955288+Bianca-Ribeiro-0@users.noreply.github.com>
2024-06-02 19:40:02 -0300

    feat: joining cep api and climate api

--- src/main/java/com/weatherAPI/ApiHandler.java
+++ src/main/java/com/weatherAPI/ApiHandler.java
@@ -9,148 +9,106 @@ import model.ViaCep;
 import java.text.DecimalFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class ApiHandler {
-    private ViaCepAPI viaCepAPI;
-    private OpenWeatherAPI openWeatherAPI;
-    private Coordenates coordenates;
+    private static final ViaCepAPI viaCepAPI = new ViaCepAPI();
+    private static final OpenWeatherAPI openWeatherAPI = new OpenWeatherAPI();
+    private static final Coordenates coordenates = new Coordenates();
 
-    public ApiHandler() {
-        this.viaCepAPI = new ViaCepAPI();
-        this.openWeatherAPI = new OpenWeatherAPI();
-        this.coordenates = new Coordenates();
-    }
-
-    public void consultarEnderecoPorCep(Scanner scanner) {
-        System.out.print("Digite o CEP: ");
-        String cep = scanner.nextLine();
-
-        ViaCep viaCep = viaCepAPI.getCepPorCep(cep);
-
-        if (viaCep != null) {
-            System.out.println("\nInformações do CEP:");
-            System.out.println("CEP: " + viaCep.getCep());
-            System.out.println("Logradouro: " + viaCep.getLogradouro());
-            System.out.println("Complemento: " + viaCep.getComplemento());
-            System.out.println("Bairro: " + viaCep.getBairro());
-            System.out.println("Localidade: " + viaCep.getLocalidade());
-            System.out.println("UF: " + viaCep.getUf());
-            System.out.println("IBGE: " + viaCep.getIbge());
-            System.out.println("GIA: " + viaCep.getGia());
-            System.out.println("DDD: " + viaCep.getDdd());
-            System.out.println("SIAFI: " + viaCep.getSiafi());
-        } else {
-            System.out.println("\nNão foi possível obter informações para o CEP especificado.");
+    public static ViaCep consultarEnderecoPorCep(String cep) {
+        try {
+            return viaCepAPI.getCepPorCep(cep);
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao consultar o CEP: " + e.getMessage());
+            return null;
         }
     }
 
-    public void consultarClimaPorEstado(Scanner scanner) {
-        List<String> states = Arrays.asList("AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO");
+    public static States consultarClimaPorEstado(String countryName) {
+        try {
+            return coordenates.buildCoordinates(countryName.trim().toUpperCase());
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao consultar o clima por estado: " + e.getMessage());
+            return null;
+        }
+    }
 
+    private static void obterInformacoesClimaticas(String latitude, String longitude) {
+        try {
+            OpenWeather climaTempo = openWeatherAPI.obterInformacoesClima(latitude, longitude);
 
-        System.out.println("Estados: " + states);
-        System.out.print("Digite o estado: ");
-        
-        //entrada do usuario, remove espaços em brancos e converte para upperCase
-        String countryName = scanner.nextLine().trim().toUpperCase();
-        States state = coordenates.buildCoordinates(countryName);
-        String latitude = state.getLatitude();
-        String longitude = state.getLongitude();
-        
-        OpenWeather climaTempo = openWeatherAPI.obterInformacoesClima(latitude, longitude);
+            if (climaTempo != null) {
+                System.out.println("\nInformações do clima:\n");
+                System.out.println("País: " + (climaTempo.getSys() != null ? climaTempo.getSys().getOrDefault("country", "não disponível") : "não disponível"));
 
-
-        if (climaTempo != null) {
-            System.out.println("\nInformações do clima:" + "\n");
-            if (climaTempo.getSys() != null && climaTempo.getSys().get("country") != null) {
-                System.out.println("País: " + climaTempo.getSys().get("country"));
-            } else {
-                System.out.println("País não disponível");
-            }
-            if (climaTempo.getTimezone() != null) {
-                int timezoneSeconds = Integer.parseInt(climaTempo.getTimezone());
+                int timezoneSeconds = climaTempo.getTimezone() != null ? Integer.parseInt(climaTempo.getTimezone()) : 0;
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-               
+
                 LocalDateTime datahoraBR = LocalDateTime.now();
                 LocalDateTime datahoraUTC = LocalDateTime.now().plusSeconds(timezoneSeconds);
 
-    
                 System.out.println("Data(UTC): " + datahoraUTC.format(formatter));
                 System.out.println("Data Local: " + datahoraBR.format(formatter) + "\n");
 
+                if (climaTempo.getWeather() != null && !climaTempo.getWeather().isEmpty()) {
+                    System.out.println("Análise sinótica:");
+                    System.out.println("Condições do tempo: " + climaTempo.getWeather().get(0).get("description"));
+
+                    double temperaturaKelvin = Double.parseDouble(climaTempo.getMain().get("temp").toString());
+                    double temperaturaMaxKelvin = Double.parseDouble(climaTempo.getMain().get("temp_max").toString());
+                    double temperaturaMinKelvin = Double.parseDouble(climaTempo.getMain().get("temp_min").toString());
+
+                    DecimalFormat df = new DecimalFormat("#.##");
+                    System.out.println("Temperatura atual: " + df.format(temperaturaKelvin - 273.15) + "°C");
+                    System.out.println("Temperatura máxima: " + df.format(temperaturaMaxKelvin - 273.15) + "°C");
+                    System.out.println("Temperatura mínima: " + df.format(temperaturaMinKelvin - 273.15) + "°C");
+
+                    System.out.println("Pressão atmosférica: " + climaTempo.getMain().getOrDefault("pressure", "Dados não disponíveis") + " hPa");
+                    System.out.println("Umidade: " + climaTempo.getMain().getOrDefault("humidity", "Dados não disponíveis") + "%");
+                    System.out.println("Velocidade do vento: " + climaTempo.getWind().getOrDefault("speed", "Dados não disponíveis") + " m/s");
+                    System.out.println("Direção do vento: " + climaTempo.getWind().getOrDefault("deg", "Dados não disponíveis") + " graus");
+                } else {
+                    System.out.println("Análise sinótica não disponível");
+                }
             } else {
-                System.out.println("Data não disponível");
+                System.out.println("\nNão foi possível obter informações do clima para o estado especificado.");
             }
-            if (climaTempo.getWeather() != null && !climaTempo.getWeather().isEmpty()) {
-                
-                System.out.println("Análise sinótica:");
-                System.out.println("Condições do tempo: " + climaTempo.getWeather().get(0).get("description"));
-
-                
-                // Convertendo as string para double 
-                double temperaturaKelvin = Double.parseDouble(climaTempo.getMain().get("temp"));
-                double temperaturaMaxKelvin = Double.parseDouble(climaTempo.getMain().get("temp_max").toString());
-                double temperaturaMinKelvin = Double.parseDouble(climaTempo.getMain().get("temp_min").toString());
-
-                double temperaturaCelsius = temperaturaKelvin - 273.15;
-                double temperaturaMaxCelsius = temperaturaMaxKelvin - 273.15;
-                double temperaturaMinCelsius = temperaturaMinKelvin  - 273.15;
-
-                DecimalFormat df = new DecimalFormat("#.##");
-                temperaturaCelsius = Double.parseDouble(df.format(temperaturaCelsius));
-                temperaturaMaxCelsius = Double.parseDouble(df.format(temperaturaMaxCelsius));
-                temperaturaMinCelsius = Double.parseDouble(df.format(temperaturaMinCelsius));
-
-                System.out.println("Temperatura atual: " + temperaturaCelsius + "°C");
-                System.out.println("Temperatura máxima: " + temperaturaMaxCelsius + "°C");
-                System.out.println("Temperatura mínima: " + temperaturaMinCelsius + "°C");
-                System.out.println("Pressão atmosférica : " + climaTempo.getMain().get("pressure") + " hPa");   
-                System.out.println("Umidade: " + climaTempo.getMain().get("humidity") + "%");  
-                System.out.println("Velocidade do vento: " + climaTempo.getWind().get("speed") + " m/s");
-                System.out.println("Direção do vento: " + climaTempo.getWind().get("deg") + " graus");
-              
-
-            } else {
-                System.out.println("Análise sinótica não disponível");
-            }
-        } else {
-            System.out.println("\nNão foi possível obter informações do clima para o estado especificado.");
-        }
-
-    }
-
-    public void consultarDados(Scanner scanner) {
-        System.out.println("Escolha a operação:");
-        System.out.println("1. Consultar Endereço por CEP");
-        System.out.println("2. Consultar Clima por Estado");
-        System.out.print("Opção: ");
-        int opcao = scanner.nextInt();
-        scanner.nextLine(); // Limpando o buffer do teclado
-
-        switch (opcao) {
-            case 1:
-                consultarEnderecoPorCep(scanner);
-                break;
-            case 2:
-                consultarClimaPorEstado(scanner);
-                break;
-            default:
-                System.out.println("Opção inválida!");
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao obter as informações climáticas: " + e.getMessage());
         }
     }
 
-    public static void main(String[] args) {
-        ApiHandler apiHandler = new ApiHandler();
-        Scanner scanner = new Scanner(System.in);
+    public static void consultaAmbos(Scanner scanner) {
+        try {
+            System.out.println("Consulta de clima através do CEP enviado");
+            System.out.println("===============================================");
+            System.out.print("Digite o CEP: ");
+            String cep = scanner.nextLine();
 
-        System.out.println("Consulta de Endereço por CEP e Clima por Estado");
-        System.out.println("===============================================");
+            ViaCep viaCep = consultarEnderecoPorCep(cep);
 
-        apiHandler.consultarDados(scanner);
+            if (viaCep != null) {
+                System.out.println("\nInformações do CEP:");
+                System.out.println("CEP: " + viaCep.getCep());
+                System.out.println("Logradouro: " + viaCep.getLogradouro());
+                System.out.println("Complemento: " + viaCep.getComplemento());
+                System.out.println("Bairro: " + viaCep.getBairro());
+                System.out.println("Localidade: " + viaCep.getLocalidade());
+                System.out.println("UF: " + viaCep.getUf());
 
-        scanner.close();
+                States state = consultarClimaPorEstado(viaCep.getUf());
+
+                if (state != null) {
+                    obterInformacoesClimaticas(state.getLatitude(), state.getLongitude());
+                } else {
+                    System.out.println("Não foi possível encontrar coordenadas para o estado.");
+                }
+            } else {
+                System.out.println("\nNão foi possível obter informações para o CEP especificado.");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro durante a consulta de ambos: " + e.getMessage());
+        }
     }
 }
--- src/main/java/com/weatherAPI/AuthUser.java
+++ src/main/java/com/weatherAPI/AuthUser.java
@@ -6,106 +6,112 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class AuthUser {
-    private HashMap<String, User> users;
+    private final HashMap<String, User> users;
 
     public AuthUser() {
         this.users = new HashMap<>();
     }
 
-    public void inicializacaoSistema(Scanner sc) {
+    public void inicializacaoSistema(Scanner scanner) {
         System.out.println("Bem-vindos! Por favor insira uma das opções abaixo: ");
         System.out.println("(1) - Login");
         System.out.println("(2) - Criar conta");
         System.out.println("(3) - Sair");
         System.out.println();
 
-        int opcao = Integer.parseInt(sc.nextLine());
-        switch (opcao) {
-            case 1:
-                login(sc);
-                break;
-            case 2:
-                cadastrar(sc);
-                break;
-            case 3:
-                System.out.println("Saindo do sistema...");
-                break;
-            default:
-                System.out.println("Opção inválida.");
-                break;
+        try {
+            int opcao = Integer.parseInt(scanner.nextLine());
+            switch (opcao) {
+                case 1:
+                    login(scanner);
+                    break;
+                case 2:
+                    cadastrar(scanner);
+                    break;
+                case 3:
+                    System.out.println("Saindo do sistema...");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Por favor, insira um número válido para escolher uma opção.");
+            inicializacaoSistema(scanner);
         }
     }
 
-    public void cadastrar(Scanner sc) {
+    public void cadastrar(Scanner scanner) {
         while (true) {
             User user = new User();
 
-            // Solicitando e validando os campos de cadastro usando métodos de acesso de User
             System.out.println("Insira as informações para realizar seu cadastro:");
-            System.out.printf("CPF (sem pontuação) --> ");
-            boolean cpfValido = user.setCpf(sc.nextLine());
+            try {
+                System.out.printf("CPF (sem pontuação) --> ");
+                boolean cpfValido = user.setCpf(scanner.nextLine());
 
-            System.out.printf("Nome --> ");
-            boolean nomeValido = user.setNome(sc.nextLine());
+                System.out.printf("Nome --> ");
+                boolean nomeValido = user.setNome(scanner.nextLine());
 
-            System.out.printf("Telefone (sem pontuação) --> ");
-            boolean telefoneValido = user.setTelefone(sc.nextLine());
+                System.out.printf("Telefone (sem pontuação) --> ");
+                boolean telefoneValido = user.setTelefone(scanner.nextLine());
 
-            System.out.printf("Senha --> ");
-            String senha = sc.nextLine();
+                System.out.printf("Senha --> ");
+                String senha = scanner.nextLine();
 
-            System.out.printf("Confirme sua senha --> ");
-            String confirmSenha = sc.nextLine();
+                System.out.printf("Confirme sua senha --> ");
+                String confirmSenha = scanner.nextLine();
 
-            boolean senhaValida = senha.equals(confirmSenha) && user.setSenha(senha);
+                boolean senhaValida = senha.equals(confirmSenha) && user.setSenha(senha);
 
-            // Validando se todos os campos são válidos
-            if (!cpfValido || !nomeValido || !telefoneValido || !senhaValida) {
-                // Se algum campo for inválido, imprime mensagem de erro e pergunta se deseja tentar novamente
-                System.out.println("Por favor, verifique as informações inseridas e tente novamente.");
-                System.out.println("Deseja tentar novamente? (s/n)");
-                String tentarNovamente = sc.nextLine();
-                if (!tentarNovamente.equalsIgnoreCase("s")) {
-                    System.out.println("Saindo do cadastro...");
+                if (!cpfValido || !nomeValido || !telefoneValido || !senhaValida) {
+                    System.out.println("Por favor, verifique as informações inseridas e tente novamente.");
+                    System.out.println("Deseja tentar novamente? (s/n)");
+                    String tentarNovamente = scanner.nextLine();
+                    if (!tentarNovamente.equalsIgnoreCase("s")) {
+                        System.out.println("Saindo do cadastro...");
+                        return;
+                    }
+                } else {
+                    users.put(user.getCpf(), user);
+                    System.out.println("\n>> CADASTRO REALIZADO COM SUCESSO!\n");
+                    new ApiHandler().consultaAmbos(scanner);
                     return;
                 }
-            } else {
-                // Adicionando o usuário ao HashMap e saindo do loop se o cadastro for bem-sucedido
-                users.put(user.getCpf(), user);
-                System.out.println("\n>> CADASTRO REALIZADO COM SUCESSO!\n");
-                ApiHandler apiHandler = new ApiHandler();
-                apiHandler.consultarClimaPorEstado(sc);
-                return;
+            } catch (Exception e) {
+                System.out.println("Ocorreu um erro durante o cadastro. Por favor, tente novamente.");
             }
         }
     }
 
-    public void login(Scanner sc) {
+    public void login(Scanner scanner) {
         while (true) {
             System.out.println("Insira as informações para realizar o login:");
-            System.out.printf("CPF (sem pontuação) --> ");
-            String cpf = sc.nextLine();
+            try {
+                System.out.printf("CPF (sem pontuação) --> ");
+                String cpf = scanner.nextLine();
 
-            System.out.printf("Senha --> ");
-            String senha = sc.nextLine();
+                System.out.printf("Senha --> ");
+                String senha = scanner.nextLine();
 
-            // Verificando se o usuário existe no HashMap e se a senha está correta (obs: só irá existir no hash enquanto o programa estiver sendo executado, caso finalizado não terá mais acesso. Sem BD)
-            if (users.containsKey(cpf)) {
-                User user = users.get(cpf);
-                if (user.getSenha().equals(senha)) {
-                    System.out.println("Login realizado com sucesso!");
-                    ApiHandler apiHandler = new ApiHandler();
-                    apiHandler.consultarClimaPorEstado(sc); //obs: to-do -> deve ser criada uma nova classe para juntar as duas apis, qnd criadas colocar esse método aqui!!
+                if (users.containsKey(cpf)) {
+                    User user = users.get(cpf);
+                    if (user.getSenha().equals(senha)) {
+                        System.out.println("Login realizado com sucesso!");
+                        new ApiHandler().consultaAmbos(scanner);
+                        return;
+                    }
+                }
+
+                System.out.println("CPF ou senha incorretos. Deseja tentar novamente? (s/n)");
+                String tentarNovamente = scanner.nextLine();
+                if (!tentarNovamente.equalsIgnoreCase("s")) {
+                    System.out.println("Saindo do login...");
+                    inicializacaoSistema(scanner);
                     return;
                 }
-            }
-
-            System.out.println("CPF ou senha incorretos. Deseja tentar novamente? (s/n)");
-            String tentarNovamente = sc.nextLine();
-            if (!tentarNovamente.equalsIgnoreCase("s")) {
-                System.out.println("Saindo do login...");
-                inicializacaoSistema(sc);
-                return;
+            } catch (Exception e) {
+                System.out.println("Ocorreu um erro durante o login. Por favor, tente novamente.");
             }
         }
     }
